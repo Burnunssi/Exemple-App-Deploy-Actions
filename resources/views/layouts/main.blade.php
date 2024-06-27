c01fb40904ba2d1277a0ad94a0a7fa907d57a8a0
João Vitor Burnunssi <j.burnunssi@aluno.ifsp.edu.br>
2024-06-26 21:42:50 -0300

    termino aula 20

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,16 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>@yield('title')</title>
+
     <!-- Fonte do Google -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 
 
     <!-- CSS Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <!-- CSS da aplicação -->
     <link rel="stylesheet" href="/css/styles.css">
     <script src="/js/script.js"></script>
@@ -46,9 +45,18 @@
             </div>
         </nav>
     </header>
-    @yield('content')
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+                @if(session('msg'))
+                <p class="msg">{{ session('msg') }}</p>
+                @endif
+                @yield('content')
+            </div>
+        </div>
+    </main>
     <footer>
-        <p>HDC Events &copy; 2024</p>
+        <p>HDC Events &copy; 2020</p>
     </footer>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,38 +4,34 @@
 
 @section('content')
 
-<h1>Algum Título</h1>
-<img src="/img/banner.jpg" alt="Banner">
-@if(10 > 15)
-<p>A condição é true</p>
-@endif
+<div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+    </form>
+</div>
 
-<p>{{ $nome }}</p>
+<div id="events-container" class="col-md-12">
+    <h2>Próximos eventos</h2>
+    <p class="subtitle">Veja os Eventos dos próximos dias</p>
 
-@if($nome == "Pedro")
-<p>O nome é pedro</p>
-@elseif($nome == "Mateus")
-<p>O nome é {{ $nome }} e ele tem {{ $idade }} anos, e trabalha como {{ $profissao }}</p>
-@else
-<p>O nome não é Pedro</p>
-@endif
+    <div id="cards-container" class="row">
+        @foreach($events as $event)
+            <div class="card col-md-3">
+                <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
+                
+                <div class="card-body">
+                    <p class="card-date">{{ date('d/m/Y', strtotime($event->date)) }}</p>
+                    <h5 class="card-title">{{ $event->title }}</h5>
+                    <p class="card-participants">X Participantes</p>
+                    <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
+                </div>
+            </div>
+        @endforeach
 
-@for($i = 0; $i < count($arr); $i++) <p>{{ $arr[$i] }} - {{$i}}</p>
-    @if($i == 2)
-    <p>O i é 2</p>
-    @endif
-    @endfor
-
-    @foreach($nomes as $nome)
-    <p>{{ $loop->index }}</p>
-    <p>{{ $nome }}</p>
-    @endforeach
-
-    @php
-    $name = "João";
-    echo $name;
-    @endphp
-
-    {{-- Comentário do Blade --}}
-
+        @if(count($events) == 0)
+            <p>Não há eventos disponíveis</p>
+        @endif
+    </div>
+</div>
 @endsection
