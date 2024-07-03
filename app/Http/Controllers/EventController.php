2196efdad164e4e83817bdf9deff6403e572250c
João Vitor Burnunssi <j.burnunssi@aluno.ifsp.edu.br>
2024-07-03 19:25:24 -0300

    até aula 24

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,18 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
 use App\Models\Event;
+use App\Models\User;
 
 class EventController extends Controller
 {
+
     public function index()
     {
-        $events = Event::all();
 
-        return view('welcome', ['events' => $events]);
+        $search = request('search');
+
+        if ($search) {
+
+            $events = Event::where([
+                ['title', 'like', '%' . $search . '%']
+            ])->get();
+        } else {
+            $events = Event::all();
+        }
+
+        return view('welcome', ['events' => $events, 'search' => $search]);
     }
 
     public function create()
@@ -23,6 +33,7 @@ class EventController extends Controller
 
     public function store(Request $request)
     {
+
         $event = new Event;
 
         $event->title = $request->title;
@@ -46,14 +57,20 @@ class EventController extends Controller
             $event->image = $imageName;
         }
 
+        $user = auth()->user();
+        $event->user_id = $user->id;
+
         $event->save();
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
     }
 
-    public function show($id){
+    public function show($id)
+    {
         $event = Event::findOrFail($id);
 
-        return view('events.show', ['event' => $event]);
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
+
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
     }
-} 
+}
